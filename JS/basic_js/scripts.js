18e0142afd874c19546b99020121aabc0c844219
Edwar-Sanz <ssanzjhon5@gmail.com>
2023-01-27 12:51:29 -0500

    Se agregan notas sobre manejo del DOM y se eliminan archivos que estaban repetidos

--- JS/basic_js/scripts.js
+++ JS/basic_js/scripts.js
@@ -96,7 +96,7 @@ function aleatorio_entre(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 //------------------------------------------------------------------------
-
+//
 
 
 
